87a7264f869182ba0d3969f1d2492710d275f6aa
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2024-01-27 19:02:50 +0545

    started the webiste creation, added tailwind

---
+++ pwebsite/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return(
+    <div>
+      <header className="text-3xl">
+        <div className="flex flex-row justify-between">
+          <p>Rajin.dev</p>
+        </div>
+      </header>
+      <h1 className="text-3xl text-bold underline">welcome to personal website</h1>
+    </div>
+  )
+}
+
+export default App;
