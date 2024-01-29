23752ae3499421d9a32a361c1741e20989c14bb2
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2024-01-29 23:15:13 +0545

    first design for the hero section

--- pwebsite/src/App.jsx
+++ pwebsite/src/App.jsx
@@ -1,12 +1,11 @@
+import Headersection from "./components/Headersection";
+import MainBody from "./components/MainBody";
+
 const App = () => {
   return(
     <div>
-      <header className="text-3xl">
-        <div className="flex flex-row justify-between">
-          <p>Rajin.dev</p>
-        </div>
-      </header>
-      <h1 className="text-3xl text-bold underline">welcome to personal website</h1>
+      <Headersection />
+      <MainBody />
     </div>
   )
 }
