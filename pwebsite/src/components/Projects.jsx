c14197f89c729016be38d30f7e5188d265635491
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2024-02-05 23:58:01 +0545

    projects page started

--- pwebsite/src/components/Projects.jsx
+++ pwebsite/src/components/Projects.jsx
@@ -15,22 +15,43 @@ let works = [
         desc: 'hello world3',
         link: 'some url3',
         demo: 'demo link3'
+    },
+    {
+        desc: 'hello world4',
+        link: 'some url4',
+        demo: 'demo link4'
+    },
+    {
+        desc: 'hello world5',
+        link: 'some url5',
+        demo: 'demo link5'
     }
 ]
 
 const Projects = () => {
     return (
-        <div>
-            {
-                works.map((work, index) => (
-                    <div key={index}>
-                        <p>{work.desc}</p>
-                        <p>{work.link}</p>
-                        <p>{work.demo}</p>
-                    </div>
-                ))
-            }
-        </div>
+        <div className='bg-zinc-300 p-4'>
+            <div className='w-4/5 mx-auto my-8 flex flex-col bg-slate-300 border-2 border-black'>
+                {
+                    works.map((work, index) => (
+                        <div key={index} className='flex odd:flex-row even:flex-row-reverse border-2 border-red-300 p-4'>
+                            <div className='flex border-2 justify-between border-blue-600'>
+                                <div className='flex'>
+                                    <img src="" alt="images" />
+                                </div>
+                                <div className='flex flex-col'>
+                                <p className='text-2xl'>{work.desc}</p>
+                                    <div className='flex flex-row gap-4'>
+                                        <button className='border-2 border-black rounded-lg'>{work.link}</button>
+                                        <button>{work.demo}</button>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                    ))
+                }
+            </div>
+    </div>
     )
 }
 
