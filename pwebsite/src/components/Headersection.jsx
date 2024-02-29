e18c1ec5089c054bbcedd537ad3b6506fe512c3b
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2024-02-29 10:08:24 +0545

    more header changes

--- pwebsite/src/components/Headersection.jsx
+++ pwebsite/src/components/Headersection.jsx
@@ -4,9 +4,9 @@ import CV from '../assets/Rajin Khatri CV.pdf'
 const Headersection = () => {
   return (
     <div className='bg-neutral-400'>
-    <div className=' w-11/12 mx-auto mb-0 lg:text-4xl sm:md:text-xl font-bold flex justify-between p-4'>
-    <p>Rajin.dev</p>
-    <div className='sm:md:text-sm lg:text-xl font-semi-bold flex justify-between gap-4  md:flex-row'>
+    <div className=' w-11/12 mx-auto mb-0 font-bold flex justify-between p-4'>
+    <p className='lg:text-4xl sm:md:text-2xl'>Rajin.dev</p>
+    <div className='sm:md:text-xs lg:text-xl font-semibold flex justify-between gap-4  md:flex-row'>
     {/* <p >Home</p>
     <p >About me</p>
     <p >Projects</p> */}
